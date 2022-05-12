a98174308ababc4540d982722d9c4cf7cce4c9eb
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2022-05-12 18:43:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -10,6 +10,11 @@ class script(object):
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+
+    ANYFILECAPTION_TXT = """**File Name:**
+`{file_name}`         
+**Size:** `{file_size}`
+`This File will be deleted after 10 minutes!
     SOURCE_TXT = """<b>NOTE:</b>
 - Eva Maria is a open source project. 
 - Source - https://github.com/EvamariaTG/EvaMaria  
