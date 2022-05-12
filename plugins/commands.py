74bb550f98969e61544ed59043bf2790bf7cd73e
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2022-05-12 18:02:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -416,16 +416,6 @@ async def settings(client, message):
             ],
             [
                 InlineKeyboardButton(
-                    'Redirect To',
-                    callback_data=f'setgs#redirect_to#{settings["redirect_to"]}#{grp_id}',
-                ),
-                InlineKeyboardButton(
-                    '👤 PM' if settings["redirect_to"] == "PM" else '📄 Chat',
-                    callback_data=f'setgs#redirect_to#{settings["redirect_to"]}#{grp_id}',
-                ),
-            ],
-            [
-                InlineKeyboardButton(
                     'Bot PM',
                     callback_data=f'setgs#botpm#{settings["botpm"]}#{grp_id}',
                 ),
